88896cd1e4f5c976e03d7754248824f9407691b2
Pablo Delgado <andresdelgadocampoverde@gmail.com>
2018-12-10 11:26:55 -0500

    commit3

--- ManejoComputadoras/src/emsamablajecomputadoras/creacional/AsusZenbookBuilder.java
+++ ManejoComputadoras/src/emsamablajecomputadoras/creacional/AsusZenbookBuilder.java
@@ -1,5 +1,31 @@
 package emsamablajecomputadoras.creacional;
 
-public class AsusZenbookBuilder {
+public class AsusZenbookBuilder extends computadorasBuilder{
+
+	@Override
+	public void DefinirComputador() {
+		pc = new Computador();
+		pc.setMarca("Asus");
+		pc.setModelo("Zenbook");
+		pc.setRam(16);
+		pc.setAlmacenamiento(500);
+		pc.setCoolerExterno(true);
+		
+	}
+
+	@Override
+	public void crearMainboard() {
+		pc.getPlaca().setModelo("Prime");
+		pc.getPlaca().setNumeracion("Z370");
+		
+	}
+
+	@Override
+	public void definirSO() {
+		pc.getOs().setArquitectura(64);
+		pc.getOs().setNombre("Windows");
+		pc.getOs().setVersion("10 Home");
+		
+	}
 
 }
